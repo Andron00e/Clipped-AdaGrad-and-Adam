e6c87bd6825d5ffd9bb62735cd6120df0111fa04
sevashasla <sevashasla@yandex.ru>
2025-06-13 14:53:02 +0200

    added run.sh (to reproduce)

--- ResNet_training/run_all_multirun.sh
+++ ResNet_training/run_all_multirun.sh
@@ -2,14 +2,14 @@
 
 if [[ " $@ " =~ " 0 " ]]; then
     echo "run sgd-layerwise-miltirun"
-    python3 multi_runs_cifar10.py --config-name resnet_parent \
+    python3 multi_runs_cifar10.py --config-name resnet_parent_multirun \
         opt.clipping="layerwise" \
         opt.max_grad_norm=5.0 \
         global_.wandb_run_name="sgd-layerwise-multirun" \
         opt.optimizer=sgd opt.lr=1e-2
 
     echo "run adam-layerwise-miltirun"
-    python3 multi_runs_cifar10.py --config-name resnet_parent \
+    python3 multi_runs_cifar10.py --config-name resnet_parent_multirun \
         opt.clipping="layerwise" \
         opt.max_grad_norm=2.5 \
         global_.wandb_run_name="adam-layerwise-miltirun" \
@@ -18,14 +18,14 @@ fi
 
 if [[ " $@ " =~ " 1 " ]]; then
     echo "run sgd-elementwise-multirun"
-    python3 multi_runs_cifar10.py --config-name resnet_parent \
+    python3 multi_runs_cifar10.py --config-name resnet_parent_multirun \
         opt.clipping="elementwise" \
         opt.max_grad_norm=2.5 \
         global_.wandb_run_name="sgd-elementwise-multirun" \
         opt.optimizer=sgd opt.lr=1e-2
 
     echo "run adam-elementwise-multirun"
-    python3 multi_runs_cifar10.py --config-name resnet_parent \
+    python3 multi_runs_cifar10.py --config-name resnet_parent_multirun \
         opt.clipping="elementwise" \
         opt.max_grad_norm=0.1 \
         global_.wandb_run_name="adam-elementwise-multirun" \
@@ -34,14 +34,14 @@ fi
 
 if [[ " $@ " =~ " 2 " ]]; then
     echo "run sgd-global-multirun"
-    python3 multi_runs_cifar10.py --config-name resnet_parent \
+    python3 multi_runs_cifar10.py --config-name resnet_parent_multirun \
         opt.clipping="global" \
         opt.max_grad_norm=5.0 \
         global_.wandb_run_name="sgd-global-multirun" \
         opt.optimizer=sgd opt.lr=1e-2
 
     echo "run adam-global-multirun"
-    python3 multi_runs_cifar10.py --config-name resnet_parent \
+    python3 multi_runs_cifar10.py --config-name resnet_parent_multirun \
         opt.clipping="global" \
         opt.max_grad_norm=1.0 \
         global_.wandb_run_name="adam-global-multirun" \
@@ -50,14 +50,14 @@ fi
 
 if [[ " $@ " =~ " 3 " ]]; then
     echo "run sgd-local-multirun"
-    python3 multi_runs_cifar10.py --config-name resnet_parent \
+    python3 multi_runs_cifar10.py --config-name resnet_parent_multirun \
         opt.clipping="local" \
         opt.max_grad_norm=2.5 \
         global_.wandb_run_name="sgd-local-multirun" \
         opt.optimizer=sgd opt.lr=1e-2
 
     echo "run adam-local-multirun"
-    python3 multi_runs_cifar10.py --config-name resnet_parent \
+    python3 multi_runs_cifar10.py --config-name resnet_parent_multirun \
         opt.clipping="local" \
         opt.max_grad_norm=0.1 \
         global_.wandb_run_name="adam-local-multirun" \
